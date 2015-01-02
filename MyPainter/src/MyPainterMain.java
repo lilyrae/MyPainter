a0d47b87bbf0e523350bb8eb3c53b02b2fc872d3
lily <lily@debian>
2015-01-02 11:27:02 +0000

    Fixed freehand lag and extended JPanel

--- MyPainter/src/MyPainterMain.java
+++ MyPainter/src/MyPainterMain.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
 
 import javax.swing.BorderFactory;
+import java.awt.Graphics;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -8,11 +11,18 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+
 
 public class MyPainterMain {
 
 	static JFrame frameMain;
-	static JPanel panelCanvas;
+	static PanelPaint panelCanvas;
 	static JButton buttonLine;
 	static JButton buttonFreehand;
 	static JButton buttonSquare;
@@ -39,10 +49,18 @@ public class MyPainterMain {
     static JMenuItem UndoAction;
     static JMenuItem FAQAction;
 	static JMenuBar menuBar;
+	//paint tools
+	static int currentX;
+	static int currentY;
+	static int initialX;
+	static int initialY;
 	
 	/**
 	 * @param args
 	 */
+	//static boolean is_clicked = false;
+	static Color g_color = Color.BLACK;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -56,70 +74,139 @@ public class MyPainterMain {
         frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //panel for drawing canvas
-        panelCanvas = new JPanel();
+        panelCanvas = new PanelPaint();
+        
         panelCanvas.setBounds(108,45,660,480);
-        panelCanvas.setBorder(BorderFactory.createLineBorder(Color.black));
-        panelCanvas.setBackground(Color.white);
+
         
         //buttons for tools
         buttonFreehand = new JButton();
         buttonFreehand.setBounds(15,45,34,34);
         
+        buttonFreehand.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
+        
         buttonLine = new JButton();
         buttonLine.setBounds(49,45,34,34);
+        
+        buttonLine.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
         
         buttonSquare = new JButton();
         buttonSquare.setBounds(15,79,34,34);
         
+        buttonSquare.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
+        
         buttonCircle = new JButton();
         buttonCircle.setBounds(49,79,34,34);
+        
+        buttonCircle.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
         
         buttonRubber = new JButton();
         buttonRubber.setBounds(15,113,34,34);
         
+        buttonRubber.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
+        
         buttonPen = new JButton();
         buttonPen.setBounds(49,113,34,34);
+        
+        buttonPen.addActionListener(new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  
+	    			//open the first screen	
+	          }
+	          });
+        
+        ActionListener colourListener = new ActionListener() {
+	          public void actionPerformed(ActionEvent e){
+	        	  g_color = ((JButton)e.getSource()).getBackground();
+	    			
+	          }
+	          };
         
         //buttons for colours
         buttonBlack = new JButton();
         buttonBlack.setBounds(15,162,34,34);
         buttonBlack.setBackground(Color.black);
+        buttonBlack.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonBlack.addActionListener(colourListener);
         
         buttonWhite = new JButton();
         buttonWhite.setBounds(49,162,34,34);
         buttonWhite.setBackground(Color.white);
+        buttonWhite.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonWhite.addActionListener(colourListener);
         
         buttonRed = new JButton();
         buttonRed.setBounds(15,196,34,34);
         buttonRed.setBackground(Color.red);
+        buttonRed.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonRed.addActionListener(colourListener);
         
         buttonYellow = new JButton();
         buttonYellow.setBounds(49,196,34,34);
         buttonYellow.setBackground(Color.yellow);
+        buttonYellow.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonYellow.addActionListener(colourListener);
         
         buttonGreen = new JButton();
         buttonGreen.setBounds(15,230,34,34);
         buttonGreen.setBackground(Color.green);
+        buttonGreen.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonGreen.addActionListener(colourListener);
         
         buttonBlue = new JButton();
         buttonBlue.setBounds(49,230,34,34);
         buttonBlue.setBackground(Color.blue);
+        buttonBlue.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonBlue.addActionListener(colourListener);
         
         buttonPurple = new JButton();
         buttonPurple.setBounds(15,264,34,34);
         buttonPurple.setBackground(Color.magenta);
+        buttonPurple.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonPurple.addActionListener(colourListener);
         
         buttonPink = new JButton();
         buttonPink.setBounds(49,264,34,34);
         buttonPink.setBackground(Color.pink);
+        buttonPink.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonPink.addActionListener(colourListener);
         
         buttonOrange = new JButton();
         buttonOrange.setBounds(15,298,34,34);
         buttonOrange.setBackground(Color.orange);
+        buttonOrange.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonOrange.addActionListener(colourListener);
         
         buttonGrey = new JButton();
         buttonGrey.setBounds(49,298,34,34);
         buttonGrey.setBackground(Color.gray);
+        buttonGrey.setBorder(BorderFactory.createLineBorder(Color.black));
+        buttonGrey.addActionListener(colourListener);
         
         setupMenuBar();
         
@@ -177,5 +264,112 @@ public class MyPainterMain {
 	        frameMain.setJMenuBar(menuBar);
 	    
 	}
-
+	
 }
+
+class PanelPaint extends JPanel{
+	private static final long serialVersionUID = 1L;
+	
+	ArrayList<Point> a_p = new ArrayList<Point>();
+	ArrayList<Color> a_c = new ArrayList<Color>();
+	ArrayList<Integer> a_i = new ArrayList<Integer>();
+	
+	boolean is_clicked = false;
+	int id_click = -1;
+	
+	public PanelPaint(){
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setBackground(Color.white);
+		//setBounds(108,45,660,480);
+		
+		addMouseMotionListener(new MouseMotionListener() {
+            public void mouseDragged(MouseEvent event) {
+            	
+            	if(is_clicked)	
+            	{
+            		repaint();
+            		a_p.add(new Point(event.getX(), event.getY()));
+            		a_c.add(MyPainterMain.g_color);
+            		a_i.add(id_click);
+            		repaint();
+            	}
+
+               
+            }
+
+
+			@Override
+			public void mouseMoved(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+        
+        
+	 
+    addMouseListener(new MouseListener() {
+        public void mousePressed(MouseEvent event) {
+        	
+        	repaint();
+        	id_click++;
+        	is_clicked = true;
+        	a_p.add(new Point(event.getX(), event.getY()));
+    		a_c.add(MyPainterMain.g_color);
+    		a_i.add(id_click);
+    		repaint();
+        }
+        public void mouseReleased(MouseEvent event) {
+
+        	is_clicked = false;
+        }
+		@Override
+		public void mouseClicked(MouseEvent event) {
+			/*// TODO Auto-generated method stub
+        	initialX = event.getX();
+        	initialY = event.getY();
+            System.out.println("initialX: " + initialX + " ,initialY: "+ initialY);*/
+			
+		}
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			is_clicked = false;
+			
+		}
+    });
+
+	}
+	
+	public Dimension getPreferredSize(){
+		return new Dimension(660,480);
+	}
+    
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        if(!a_p.isEmpty())
+        {
+        	g.setColor(a_c.get(0));
+        	g.drawLine(a_p.get(0).x, a_p.get(0).y, a_p.get(0).x, a_p.get(0).y);
+        }
+        
+		for(int i = 0; i < a_p.size()-1; i++){
+			g.setColor(a_c.get(i));
+			
+			if(a_i.get(i) == a_i.get(i+1))
+				g.drawLine(a_p.get(i).x, a_p.get(i).y, a_p.get(i+1).x, a_p.get(i+1).y);
+			else
+				g.drawLine(a_p.get(i).x, a_p.get(i).y, a_p.get(i).x, a_p.get(i).y);
+			
+		}
+
+    } 
+
+       
+}
+
